35679dc1403bacc01533f43328af27022b7bc40f
carward25 <carward@ucsc.edu>
2021-10-26 18:46:19 -0700

    Initial commit

---
+++ multiply.py
@@ -0,0 +1,11 @@
+#name: Cassidy Ward
+#date: 10/26/2021
+#description: this program takes two positive integers as
+# parameters and returns the product of those two numbers
+
+
+def multiply(n1, n2):
+    if n2 == 0:  # base case, if n2 is 0
+        return 0  # then return 0
+    else:  # recursive case
+        return n1 + multiply(n1, n2 - 1)  # n1 times n2 = n1 + (n1 times (n2-1))
